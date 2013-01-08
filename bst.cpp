dea1a19e6fce29c88a2321b796afc10aa404ac24
emil <emil@emil-pc.(none)>
2013-01-09 00:09:43 +0530

    2nd commit

--- bst.cpp
+++ bst.cpp
@@ -8,6 +8,7 @@ struct tree {
   struct tree *parent;
 };
 typedef struct tree Tree;
+
 void insert(Tree **root, int data) {
   Tree *node = (Tree*)malloc(sizeof(Tree));
   node->data = data;
@@ -22,12 +23,26 @@ void insert(Tree **root, int data) {
   Tree *child = *root;
   while(child != NULL) {
     parent = child;
-    if (data < parent) child = parent->left;
+    if (data < parent->data) child = parent->left;
     else child = parent->right;
+  }
+  if (data < parent->data) parent->left = node;
+  else parent->right = node;
+}
+
+void traversal(Tree *n) {
+  if (n != NULL) {
+    traversal(n->left);
+    printf("%d\n",n->data);
+    traversal(n->right);
   }
 }
 int main() {
   Tree *root = NULL;
   insert(&root, 10);
+  insert(&root, 20);
+  insert(&root, 5);
+  insert(&root, 7);
+  traversal(root);
   return 0;
 }
